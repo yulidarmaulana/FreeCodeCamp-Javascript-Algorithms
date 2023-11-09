dd04b820899cd92f1a00aa1c3495f05bf278754b
yulidarmaulana <31232250+yulidarmaulana@users.noreply.github.com>
2023-11-09 07:58:53 +0700

    update stage course from 54 to 77

---
+++ 056assignmentWithaReturnedValue.js
@@ -0,0 +1,12 @@
+// Setup
+let processed = 0;
+
+function processArg(num) {
+  return (num + 3) / 5;
+}
+
+// Only change code below this line
+// change = change(10);
+
+// setup
+processed = processArg(7);
---
+++ 059useConditionalLogicWithIfStatements.js
@@ -0,0 +1,11 @@
+function trueOrFalse(wasThatTrue) {
+    // Only change code below this line
+  
+  
+  
+    // Only change code above this line
+    if(wasThatTrue) {
+      return "Yes, that was true";
+    }
+      return "No, that was false";
+  }
